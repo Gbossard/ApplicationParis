a7051f62e23bf604baba6ebda9c6484cb1781eda
Gaëlle Bossard <bossard.gaelle@gmail.com>
2024-12-03 13:18:57 +0100

    ParisUiState: Add isShowingHomePage value

--- app/src/main/java/com/example/applicationparis/ui/ParisUiState.kt
+++ app/src/main/java/com/example/applicationparis/ui/ParisUiState.kt
@@ -7,7 +7,8 @@ import com.example.applicationparis.data.Place
 data class ParisUiState(
     val places: Map<MenuItemType, List<Place>> = emptyMap(),
     val currentScreens: MenuItemType = MenuItemType.RESTAURANT,
-    val currentSelectedPlace: Place = LocalPlacesDataProvider.defaultPlace
+    val currentSelectedPlace: Place = LocalPlacesDataProvider.defaultPlace,
+    val isShowingHomepage: Boolean = true
 ) {
     val currentPlaces: List<Place> by lazy { places[currentScreens]!! }
 }
